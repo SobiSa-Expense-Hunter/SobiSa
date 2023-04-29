f36271187c79a7fd27e98b7dac0dbf4963386d97
SEO <108770949+Seo0H@users.noreply.github.com>
2023-04-29 19:57:40 +0900

    [#5] chore: prettierrc세팅 추가 및 적용

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
-import "@/styles/globals.css";
-import React from "react";
+import '@/styles/globals.css';
+import React from 'react';
 
-import type { AppProps } from "next/app";
+import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
   // eslint-disable-next-line react/jsx-props-no-spreading
