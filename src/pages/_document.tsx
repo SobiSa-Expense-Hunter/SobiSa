fb3bbfbd3068f21774e10bf28f46af8b5cd87d32
SEO <108770949+Seo0H@users.noreply.github.com>
2023-04-29 19:57:40 +0900

    [#5] chore: prettierrc세팅 추가 및 적용

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,10 @@
-import React from "react";
+import React from 'react';
 
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang='en'>
       <Head />
       <body>
         <Main />
