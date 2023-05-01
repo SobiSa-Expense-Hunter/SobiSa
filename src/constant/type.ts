3ca6bfc1e76cbe98f3649c0bbe8eb21bc80d787f
SEO <108770949+Seo0H@users.noreply.github.com>
2023-05-02 02:59:12 +0900

    [#2] feature: 네이버 쇼핑 API Interface 구체화
    네이버 API 받아오는 데이터 타입 그대로 interface로 정의한것을 추가

--- src/constant/type.ts
+++ src/constant/type.ts
@@ -1,9 +1,34 @@
-export interface NaverProductResponse {
-  title: string;
-  link: string;
+// export interface NaverProductResponse {
+//   title: string;
+//   link: string;
+//   image: string;
+//   price: number;
+//   productType: number;
+// }
+
+export interface NaverProductItemResponse {
+  brand: string;
+  category1: string;
+  category2: string;
+  category3: string;
+  category4: string;
+  price: string;
   image: string;
-  price: number;
-  productType: number;
+  link: string;
+  lprice: string;
+  maker: string;
+  mallName: string;
+  productId: string;
+  productType: string;
+  title: string;
+}
+
+export interface NaverProductResponse {
+  items: NaverProductItemResponse[];
+  display: string;
+  lastBuildDate: string;
+  start: string;
+  total: string;
 }
 
 export interface Product {
