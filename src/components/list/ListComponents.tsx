9d99c471da235a75593f051a5876ab617872d94b
DESKTOP-1V9OQ1L\GIGABYTE <emma05127@gmail.com>
2023-05-02 14:50:26 +0900

    [#2] style: 물건 리스트를 구분할 수 있도록 스타일 정리

--- src/components/list/ListComponents.tsx
+++ src/components/list/ListComponents.tsx
@@ -1,21 +1,42 @@
-import { useState } from 'react';
+import styled from 'styled-components';
 
 import useSearchProduct from '@/hooks/useSearchProduct';
 
 const ListComponents = () => {
   const userSearch = '빵';
   const { products, isLoading } = useSearchProduct(userSearch);
-  const [userInput, setUserInput] = useState('');
 
   if (isLoading || products === undefined) return <p>로딩중..</p>;
 
   return (
-    <>
+    <DummyWrapper>
       {products.map(product => (
-        <p key={`${product.title} key`}>{product.title}</p>
+        <StyledListBox key={`${product.productId} key`}>
+          <StyledImgWrapper>
+            <img src={product.image} alt={`${product.productId}img`} />
+          </StyledImgWrapper>
+          <p>{product.title}</p>
+          <p>{product.price} 원</p>
+        </StyledListBox>
       ))}
-    </>
+    </DummyWrapper>
   );
 };
 
 export default ListComponents;
+
+const StyledListBox = styled.div`
+  background: #ffffff;
+  box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.25);
+  border-radius: 10px;
+`;
+
+const StyledImgWrapper = styled.div`
+  img {
+    max-width: 50px;
+  }
+`;
+
+const DummyWrapper = styled.div`
+  height: calc(var(--vh, 1vh) * 100);
+`;
