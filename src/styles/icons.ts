28505e90e6b752d0ecae53e2db12636c77490be3
Shinpanda <duldulgo1@gmail.com>
2023-05-05 23:39:45 +0900

    feature: buttons, icon 스타일 추가

---
+++ src/styles/icons.ts
@@ -0,0 +1,21 @@
+import HamburgerIcon from '@/assets/icons/hamburger.svg';
+import LeftIcon from '@/assets/icons/left.svg';
+import LinkIcon from '@/assets/icons/link.svg';
+import LoadingIcon from '@/assets/icons/loading.svg';
+import NoticeIcon from '@/assets/icons/notice.svg';
+import TopIcon from '@/assets/icons/top.svg';
+import KakaoIcon from '@/assets/social/Kakao.svg';
+import FaceBookIcon from '@/assets/social/facebook.svg';
+import TwitterIcon from '@/assets/social/twitter.svg';
+
+export {
+  HamburgerIcon,
+  LeftIcon,
+  LinkIcon,
+  LoadingIcon,
+  NoticeIcon,
+  TopIcon,
+  KakaoIcon,
+  FaceBookIcon,
+  TwitterIcon,
+};
--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,6 +1,58 @@
-import styled from 'styled-components';
+import styled, { createGlobalStyle } from 'styled-components';
 
-import * as Buttons from './buttons';
+const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-family: 'Pretendard';
+    src: url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff') format('woff');
+    font-weight: 400;
+    font-style: normal;
+  }
+
+  html, body, div, span, applet, object, iframe,
+  h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+  a, abbr, acronym, address, big, cite, code,
+  del, dfn, em, img, ins, kbd, q, s, samp,
+  small, strike, strong, sub, sup, tt, var,
+  b, u, i, center,
+  dl, dt, dd, ol, ul, li,
+  fieldset, form, label, legend,
+  table, caption, tbody, tfoot, thead, tr, th, td,
+  article, aside, canvas, details, embed, 
+  figure, figcaption, footer, header, hgroup, 
+  menu, nav, output, ruby, section, summary,
+  time, mark, audio, video {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    font-size: 100%;
+    font: inherit;
+    font-family: '';
+    vertical-align: baseline;
+  }
+  /* HTML5 display-role reset for older browsers */
+  article, aside, details, figcaption, figure, 
+  footer, header, hgroup, menu, nav, section {
+    display: block;
+  }
+  body {
+    line-height: 1;
+  }
+  ol, ul {
+    list-style: none;
+  }
+  blockquote, q {
+    quotes: none;
+  }
+  blockquote:before, blockquote:after,
+  q:before, q:after {
+    content: '';
+    content: none;
+  }
+  table {
+    border-collapse: collapse;
+    border-spacing: 0;
+  }
+`;
 
 const Header = styled.header`
   display: flex;
@@ -27,4 +79,4 @@ const Header = styled.header`
   }
 `;
 
-export { Header, Buttons };
+export { Header, GlobalStyle };
