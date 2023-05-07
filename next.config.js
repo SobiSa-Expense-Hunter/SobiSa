9bbed14852d012e6970c5a3e40c5603792ebe2ec
SEO <108770949+Seo0H@users.noreply.github.com>
2023-05-07 22:54:34 +0900

    [#18] chore: svg import를 위한 @svgr/webpack 설정
    https://react-svgr.com/docs/next/ 참고하여 수정

--- next.config.js
+++ next.config.js
@@ -18,11 +18,23 @@ const nextConfig = {
     ];
   },
   webpack: config => {
-    config.module.rules.push({
-      test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
-      use: ['@svgr/webpack'],
-    });
+    const fileLoaderRule = config.module.rules.find(rule => rule.test?.test?.('.svg'));
+    config.module.rules.push(
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/, // *.svg?url
+      },
+      {
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        resourceQuery: { not: /url/ },
+        use: ['@svgr/webpack'],
+      },
+    );
+
+    fileLoaderRule.exclude = /\.svg$/i;
+
     return config;
   },
 };
