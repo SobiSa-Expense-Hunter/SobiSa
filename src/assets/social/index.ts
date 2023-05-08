6f8d8ba634cc7cc132d6ca1c0541719d9dbd8819
DESKTOP-1V9OQ1L\GIGABYTE <emma05127@gmail.com>
2023-05-08 15:40:48 +0900

    [#16] fix: svg imoprt시 index.ts에서 default 경로 매핑

---
+++ src/assets/social/index.ts
@@ -0,0 +1,4 @@
+/* eslint-disable import/prefer-default-export */
+export { default as FaceBookIcon } from './facebook.svg';
+export { default as KakaoIcon } from './kakao.svg';
+export { default as TwitterIcon } from './twitter.svg';
--- src/styles/icons.ts
+++ src/styles/icons.ts
@@ -1,12 +1,12 @@
-import HamburgerIcon from '@/assets/icons/hamburger.svg';
-import LeftIcon from '@/assets/icons/left.svg';
-import LinkIcon from '@/assets/icons/link.svg';
-import LoadingIcon from '@/assets/icons/loading.svg';
-import NoticeIcon from '@/assets/icons/notice.svg';
-import TopIcon from '@/assets/icons/top.svg';
-import KakaoIcon from '@/assets/social/Kakao.svg';
-import FaceBookIcon from '@/assets/social/facebook.svg';
-import TwitterIcon from '@/assets/social/twitter.svg';
+import {
+  HamburgerIcon,
+  LeftIcon,
+  LinkIcon,
+  LoadingIcon,
+  NoticeIcon,
+  TopIcon,
+} from '@/assets/icons';
+import { KakaoIcon, FaceBookIcon, TwitterIcon } from '@/assets/social';
 
 export {
   HamburgerIcon,
