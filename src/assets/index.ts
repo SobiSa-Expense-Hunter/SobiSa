a4eed872e1fc7b31d8df035eeb8bdfd885124223
Shinpanda <duldulgo1@gmail.com>
2023-05-08 16:14:47 +0900

    feature: image에 대해 index.ts 추가
    icons  와  buttons와 같이 포맷 유지

---
+++ src/assets/index.ts
@@ -0,0 +1,4 @@
+/* eslint-disable import/prefer-default-export */
+export { default as MainImage } from './image.svg';
+export { default as LineImage } from './image_line.svg';
+export { default as ItemImage } from './image_item.svg';
--- src/pages/design.tsx
+++ src/pages/design.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-import MainImage from '@/assets/image.svg';
-import ItemImage from '@/assets/image_item.svg';
-import LineImage from '@/assets/image_line.svg';
+import styled from 'styled-components';
+
+import { ItemImage, LineImage, MainImage } from '@/assets';
 import {
   BottomButton,
   ModalButton,
@@ -22,43 +22,118 @@ import {
 
 const design = () => {
   return (
-    <div>
-      <div>
-        <h2>buttons</h2>
-        <br />
-        <ModalButton>STATIC</ModalButton>
-        <ModalSubButton>STATIC</ModalSubButton>
-        <br />
-        <br />
-        <ModalLongButton>STATIC</ModalLongButton>
-        <br />
-        <br />
-        <BottomButton>STATIC</BottomButton>
-        <br />
-        <br />
-        <ShareButton>STATIC</ShareButton>
-        <br />
-        <br />
-      </div>
-      <div>
-        <h2>Image</h2>
-        <MainImage />
-        <LineImage />
-        <ItemImage />
-      </div>
-      <div>
-        <h2>Icon</h2>
-        <NoticeIcon />
-        <TopIcon />
-        <LoadingIcon />
-        <br />
-        <FaceBookIcon />
-        <TwitterIcon />
-        <KakaoIcon />
-        <LinkIcon />
-      </div>
-    </div>
+    <main>
+      <aside>
+        <ul>
+          <li>buttons</li>
+          <li>image</li>
+        </ul>
+      </aside>
+      <section>
+        <Section>
+          <h2>buttons</h2>
+          <ItemContent>
+            <h3>모달 버튼</h3>
+            <div>
+              <ModalButton>STATIC</ModalButton>
+              <ModalSubButton>STATIC</ModalSubButton>
+            </div>
+            <div>
+              <Code>&lt;ModalButton&gt;STATIC&lt;/ModalButton&gt;</Code>
+            </div>
+          </ItemContent>
+          <ItemContent>
+            <ModalLongButton>STATIC</ModalLongButton>
+          </ItemContent>
+          <ItemContent>
+            <BottomButton>STATIC</BottomButton>
+          </ItemContent>
+          <ItemContent>
+            <ShareButton>STATIC</ShareButton>
+          </ItemContent>
+        </Section>
+        <Section>
+          <h2>Image</h2>
+          <ItemContent>
+            <MainImage />
+          </ItemContent>
+          <ItemContent>
+            <LineImage />
+          </ItemContent>
+          <ItemContent>
+            <ItemImage />
+          </ItemContent>
+        </Section>
+        <Section>
+          <h2>Icon</h2>
+          <ItemContent>
+            <NoticeIcon />
+          </ItemContent>
+          <ItemContent>
+            <TopIcon />
+          </ItemContent>
+          <ItemContent>
+            <LoadingIcon />
+          </ItemContent>
+          <ItemContent>
+            <FaceBookIcon />
+          </ItemContent>
+          <ItemContent>
+            <TwitterIcon />
+          </ItemContent>
+          <ItemContent>
+            <KakaoIcon />
+          </ItemContent>
+          <ItemContent>
+            <LinkIcon />
+          </ItemContent>
+        </Section>
+      </section>
+    </main>
   );
 };
 
 export default design;
+
+const Main = styled.main`
+  display: grid;
+  grid: 1 / 5;
+  counter-reset: sec-counter;
+`;
+
+const Section = styled.section`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  background: #fefefe;
+  padding: 10px;
+  border: 1px solid darkgray;
+  border-radius: 5px;
+  margin: 10px;
+  h2 {
+    text-align: center;
+    font-size: 20px;
+    font-weight: bold;
+  }
+  h2::before {
+    counter-increment: sec-counter;
+    content: counter(sec-counter) '. ';
+  }
+`;
+
+const ItemContent = styled.article`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  h3 {
+    font-weight: bold;
+  }
+`;
+
+const Code = styled.code`
+  background-color: #eee;
+  border-radius: 3px;
+  font-family: courier, monospace;
+  padding: 0 3px;
+  font-size: 0.9rem;
+`;
