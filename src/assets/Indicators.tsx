dac51fa1663ae00c92c212a87cb4a5e92c21b168
SEO <108770949+Seo0H@users.noreply.github.com>
2023-05-15 13:55:50 +0900

    [#16] fix: Indicators 액박 해결
    I -> i 소문자 변경
    추가로 인디케이터 alt 수정

--- src/assets/Indicators.tsx
+++ src/assets/Indicators.tsx
@@ -1,27 +1,27 @@
 import Image from 'next/image';
 
-const INDICATOR_PATH = '/assets/Indicator';
+const INDICATOR_PATH = '/assets/indicator';
 
 export const Indicator01 = () => {
   return (
-    <Image src={`${INDICATOR_PATH}/Indicator01.svg`} width={43} height={4} alt='소비사 캐릭터' />
+    <Image src={`${INDICATOR_PATH}/indicator01.svg`} width={43} height={4} alt='인디케이터 1' />
   );
 };
 
 export const Indicator02 = () => {
   return (
-    <Image src={`${INDICATOR_PATH}/Indicator02.svg`} width={43} height={4} alt='소비사 캐릭터' />
+    <Image src={`${INDICATOR_PATH}/indicator02.svg`} width={43} height={4} alt='인디케이터 2' />
   );
 };
 
 export const Indicator03 = () => {
   return (
-    <Image src={`${INDICATOR_PATH}/Indicator03.svg`} width={43} height={4} alt='소비사 캐릭터' />
+    <Image src={`${INDICATOR_PATH}/indicator03.svg`} width={43} height={4} alt='인디케이터 3' />
   );
 };
 
 export const Indicator04 = () => {
   return (
-    <Image src={`${INDICATOR_PATH}/Indicator04.svg`} width={43} height={4} alt='소비사 캐릭터' />
+    <Image src={`${INDICATOR_PATH}/indicator04.svg`} width={43} height={4} alt='인디케이터 4' />
   );
 };
