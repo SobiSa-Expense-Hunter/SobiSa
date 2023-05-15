23e57a4aa738b7d1be71dafb0e4b15b002d98229
DESKTOP-1V9OQ1L\GIGABYTE <emma05127@gmail.com>
2023-05-15 20:00:30 +0900

    [#47] feature: meta tag 추가
    naver 참고하여 작업
    추후, apple-touch-icon 같은 경우 어플 로고에 맞게 디자인 작업 필요
    (현재 임시로 넣어둠)

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -45,6 +45,21 @@ class MyDocument extends Document {
             href='https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.6/dist/web/variable/pretendardvariable-dynamic-subset.css'
           />
           <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+
+          <title>소비사!: 소비사냥꾼</title>
+
+          <meta name='apple-mobile-web-app-title' content='소비사!: 소비사냥꾼' />
+          <meta name='description' content='당신의 소비를 막아주는 소비사냥꾼' />
+
+          <meta name='og:title' content='소비사!: 소비 사냥꾼' />
+          <meta name='og:url' content='https://sobisa.vercel.app/' />
+          <meta name='og:image' content='' />
+          <meta name='og:description' content='당신의 소비를 막아주는 소비사냥꾼' />
+          <meta name='og:type' content='website' />
+
+          <meta name='twitter:title' content='소비사!: 소비 사냥꾼' />
+
+          <link rel='apple-touch-icon' sizes='114x114' href='assets/image/sobisa_logo.png' />
         </Head>
         <body>
           <div id='modal-root' />
