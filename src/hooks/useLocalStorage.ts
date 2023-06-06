ffdf8af8664aaa2beba4f01a9e889ba8f042fd7b
SEO <108770949+Seo0H@users.noreply.github.com>
2023-06-07 03:10:12 +0900

    [#108] fix: useLocalStorage hook의 key, value 타입 제한
    현재 사용하는 localStorage 변수들이 string 이기에, 해당 타입으로 제한

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,19 +1,17 @@
 import { useState } from 'react';
 
-const useLocalStorage = <T>(key: string, initialValue: T): [T, (value: T) => void] => {
-  const [state, setState] = useState<T>(() => {
+const useLocalStorage = (key: string, initialValue: string): [string, (value: string) => void] => {
+  const [state, setState] = useState(() => {
     try {
       const value = window.localStorage.getItem(key);
-      return value ? JSON.parse(value) : initialValue;
+      return value || initialValue;
     } catch (error) {
-      return error as T;
+      return error as string;
     }
   });
 
-  const setValue = (value: T | ((prevState: T) => T)) => {
-    const valueToStore =
-      typeof value === 'function' ? (value as (prevState: T) => T)(state) : value;
-    window.localStorage.setItem(key, JSON.stringify(valueToStore));
+  const setValue = (value: string) => {
+    window.localStorage.setItem(key, value);
     setState(value);
   };
 
