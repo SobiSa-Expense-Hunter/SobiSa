101fd9dd26ceb8fab9f601f9add6a303db44cb3e
DESKTOP-1V9OQ1L\GIGABYTE <emma05127@gmail.com>
2023-06-07 19:58:44 +0900

    [#108] style: CustomerBox img 교체 및 여백 추가

--- src/components/about/CustomerBox.tsx
+++ src/components/about/CustomerBox.tsx
@@ -15,12 +15,12 @@ function CustomerBox({ imgNum, mainText, subText, subTextOrange }: CustomerProps
   return (
     <StyledHStack height='94px' alignItems='center' padding='0 16px'>
       <Image
-        src={`assets/image/about/customer/${imgNum}.png`}
+        src={`assets/image/about/customer/profile_${imgNum}.png`}
         width={54}
         height={54}
         alt={`고민하는 소비자 이모지 ${imgNum}`}
       />
-      <Layout.VStack>
+      <Layout.VStack margin='0 0 0 16px'>
         <Font.Large>{mainText}</Font.Large>
         <Layout.Box height='12px' />
         <Layout.Box>
