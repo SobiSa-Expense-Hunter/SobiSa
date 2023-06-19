1b07008efcfdda0c4ba6a7b4f3043e6ba6235618
Shinpanda <duldulgo1@gmail.com>
2023-06-19 12:44:35 +0900

    [#147] style: 사용하지 않는 라이브러리 정리

--- src/components/common/Toast.tsx
+++ src/components/common/Toast.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 
-import { AnimatePresence, motion, usePresence } from 'framer-motion';
+import { AnimatePresence, motion } from 'framer-motion';
 import styled from 'styled-components';
 
 const ToastItem = ({
