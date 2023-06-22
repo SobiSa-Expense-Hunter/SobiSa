1be68ebb8fc0b72f633e21bccb64adbf51ee60be
seo <emma05127@gmail.com>
2023-06-22 16:34:57 +0900

    [#161] feature: SearchInput autocomplete 데이터 상태 추가
    - 자동완성 데이터가 로딩, 성공, 에러 3가지의 상태를 가지도록 추가
    - 해당 상태에 따른 UI변경 구현

--- src/components/common/SearchInput/index.tsx
+++ src/components/common/SearchInput/index.tsx
@@ -1,18 +1,16 @@
 /* eslint-disable consistent-return */
 import { useState, useRef, useEffect } from 'react';
-import type { Dispatch, SetStateAction } from 'react';
 
 import { NextRouter, useRouter } from 'next/router';
-import styled from 'styled-components';
-import { v4 as uuid } from 'uuid';
 
 import { MagnifyingGlassIcon } from '@/assets/Icons';
-import * as Layout from '@/components/common/layout/index';
+import * as Style from '@/components/common/SearchInput/style';
 import NoticeModal from '@/components/modal/NoticeModal';
 import useAutoCmp from '@/hooks/useAutoCmp';
 import useNoticeModal from '@/hooks/useNoticeModal';
-import * as Font from '@/styles/font';
 import detectMobileDevice from '@/utils/checkMobileDevice';
+
+import AutoCmpContainer from './AutoCmpContainer';
 
 function SearchInput() {
   const router = useRouter();
@@ -20,7 +18,7 @@ function SearchInput() {
   const [isFocus, setIsFocus] = useState(false);
   const { modalState, dispatchModalState } = useNoticeModal();
   const inputRef = useRef<HTMLInputElement>(null);
-  const { autoCmpList } = useAutoCmp(search);
+  const { autoCmpList, state } = useAutoCmp(search);
 
   useEffect(() => {
     const isMobile = detectMobileDevice(window.navigator.userAgent);
@@ -38,10 +36,11 @@ function SearchInput() {
       dispatchModalState({ type: 'SHOW', message: error.message });
     }
   };
+
   return (
-    <SearchInputContainer>
-      <SearchInputBox>
-        <SearchEnterInput
+    <Style.SearchInputContainer>
+      <Style.SearchInputBox>
+        <Style.SearchEnterInput
           type='text'
           placeholder='살까 말까하는 그 물건...'
           value={search}
@@ -52,12 +51,12 @@ function SearchInput() {
           ref={inputRef}
         />
         {isFocus && search && autoCmpList.length > 1 && (
-          <AutoCmpContainer autoCmpList={autoCmpList} setSearch={setSearch} />
+          <AutoCmpContainer autoCmpList={autoCmpList} setSearch={setSearch} state={state} />
         )}
-      </SearchInputBox>
-      <SearchButton type='button' onClick={onSearch}>
+      </Style.SearchInputBox>
+      <Style.SearchButton type='button' onClick={onSearch}>
         <MagnifyingGlassIcon />
-      </SearchButton>
+      </Style.SearchButton>
 
       {modalState.show && (
         <NoticeModal
@@ -65,28 +64,7 @@ function SearchInput() {
           message={modalState.message}
         />
       )}
-    </SearchInputContainer>
-  );
-}
-
-interface AutoCmpProps {
-  autoCmpList: string[];
-  setSearch: Dispatch<SetStateAction<string>>;
-}
-
-function AutoCmpContainer({ autoCmpList, setSearch }: AutoCmpProps) {
-  return (
-    <Relative>
-      <Absolute>
-        {autoCmpList.map(autoCmp => {
-          return (
-            <AutoCmp justifyContent='center' key={uuid()} onMouseDown={() => setSearch(autoCmp)}>
-              <Font.Medium>{autoCmp}</Font.Medium>
-            </AutoCmp>
-          );
-        })}
-      </Absolute>
-    </Relative>
+    </Style.SearchInputContainer>
   );
 }
 
@@ -101,105 +79,5 @@ function checkSearchWord(search: string) {
   if (isSpecialCharIncluded) throw new Error('특수문자를 제외하고 입력해주세요.');
   return search.trim();
 }
-
-const AutoCmp = styled(Layout.VStack)`
-  width: 100%;
-  height: 1.5em;
-  border: gray 1px;
-  padding: 0 20px;
-
-  :hover {
-    cursor: pointer;
-    background-color: ${({ theme }) => theme.colors.gray[1]};
-  }
-`;
-
-const Relative = styled.div`
-  position: relative;
-  width: 100%;
-
-  /* 위치 보정 */
-  top: -10px;
-  left: -2px;
-
-  z-index: 1;
-`;
-
-const Absolute = styled.div`
-  position: absolute;
-  width: calc(100% + 4px);
-
-  padding: 10px 0;
-
-  background-color: white;
-  border: solid ${({ theme }) => theme.colors.mainColor};
-  border-width: 0 2px 2px 2px;
-  border-radius: 0 0 10px 10px;
-`;
-
-const SearchInputContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-
-  box-sizing: border-box;
-  width: 100%;
-  height: 60px;
-  border: 2px solid ${({ theme }) => theme.colors.mainColor};
-  border-radius: 8px;
-  max-width: 310px;
-  justify-content: space-between;
-  background-color: ${({ theme }) => theme.colors.mainColor};
-`;
-
-const SearchInputBox = styled.div`
-  width: 80%;
-  height: 100%;
-  background-color: white;
-  border-radius: 6px 0 0 6px;
-`;
-
-const SearchEnterInput = styled.input`
-  text-overflow: ellipsis;
-  overflow: hidden;
-  white-space: nowrap;
-  border-radius: 6px 0 0 6px;
-  outline: none;
-  border: none;
-  width: 100%;
-  height: 100%;
-  padding: 20px;
-  max-width: 210px;
-  color: ${({ theme }) => theme.colors.mainColor};
-
-  ::placeholder,
-  ::-webkit-input-placeholder {
-    color: ${({ theme }) => theme.colors.gray[3]};
-  }
-  :-ms-input-placeholder {
-    color: ${({ theme }) => theme.colors.gray[3]};
-  }
-
-  /*INPUT-FONT */
-  font-family: 'Pretendard Variable';
-  font-weight: 500;
-  font-size: 16px;
-  line-height: 150%;
-  letter-spacing: -0.022em;
-`;
-
-const SearchButton = styled.button`
-  box-sizing: border-box;
-  display: inline-flex;
-  align-items: center;
-  justify-content: center;
-  width: 20%;
-  height: 100%;
-  padding: 0;
-  background-color: ${({ theme }) => theme.colors.mainColor};
-  border: none;
-  border-radius: 0 5px 5px 0;
-  cursor: pointer;
-`;
 
 export default SearchInput;
--- src/hooks/useAutoCmp.ts
+++ src/hooks/useAutoCmp.ts
@@ -5,15 +5,36 @@ import { getNaverShopAutoCmp, getNaverMainAutoCmp } from '@/utils/api/getAutoCmp
 
 import type { NaverMainAutoCmp, NaverShopAutoCmp } from '@/types/autoCmp';
 
+export const STATE = {
+  SUCCESS: 'success',
+  LOADING: 'loading',
+  ERROR: 'error',
+} as const;
+
 function useAutoCmp(params: string) {
   const [autoCmpList, setAutoCmpList] = useState(['']);
+  const [state, setState] = useState<(typeof STATE)[keyof typeof STATE]>(STATE.LOADING);
 
   const AUTO_CMP_MODE = { SHOP: 'naverShop', MAIN: 'naverMain' } as const;
   const mode = AUTO_CMP_MODE.SHOP; // 자동완성 api 모드 설정 부분.
 
   useEffect(() => {
     if (!params) return;
-    fetchData().then(res => setAutoCmpList(makeAutoCmpList(res).slice(0, 8)));
+    /**
+     * !DESCRIBE : 로딩스피너가 너무 빠르게 동작하면 깜빡거리듯 보여지기에 0.5초가 지나고 로딩 상태로 변경
+     */
+    const loadingTimeout = setTimeout(() => setState(STATE.LOADING), 500);
+
+    try {
+      fetchData().then(res => {
+        setAutoCmpList(makeAutoCmpList(res).slice(0, 8));
+        clearTimeout(loadingTimeout);
+        setState(STATE.SUCCESS);
+      });
+    } catch (error) {
+      clearTimeout(loadingTimeout);
+      setState(STATE.ERROR);
+    }
   }, [params, mode]);
 
   async function fetchData() {
@@ -42,7 +63,7 @@ function useAutoCmp(params: string) {
     return cleanData;
   }
 
-  return { autoCmpList };
+  return { autoCmpList, state };
 }
 
 export default useAutoCmp;
