2487fe3d52d6f9746d5c7931e7e67ded0b5661d0
SEO <108770949+Seo0H@users.noreply.github.com>
2023-06-27 02:25:21 +0900

    [#171] feature: useLocalStorage spec 작성
    해당 spec 코드를 작성하기 위해 useLocalStorage 폴더 생성

--- src/hooks/useLocalStorage/index.spec.ts
+++ src/hooks/useLocalStorage/index.spec.ts
@@ -1,6 +1,6 @@
 import { renderHook } from '@testing-library/react';
 
-import useLocalStorage from './useLocalStorage';
+import useLocalStorage from '@/hooks/useLocalStorage';
 
 describe('useLocalStorage 정상 작동 테스트', () => {
   it('key와 velue를 저장하면 로컬스토리지에 저장이 되어야 한다', () => {
