91b1f1ecf8e7e399f9d9390509cf4a8348968405
seo <emma05127@gmail.com>
2023-06-26 20:06:46 +0900

    fix: customRender SearchProvider 추가

--- src/__test__/utils/customRender.tsx
+++ src/__test__/utils/customRender.tsx
@@ -4,12 +4,15 @@ import 'jest-styled-components';
 import { render } from '@testing-library/react';
 import { ThemeProvider } from 'styled-components';
 
+import SearchProvider from '@/components/SearchProvider';
 import theme from '@/styles/theme';
 
 import type { RenderOptions } from '@testing-library/react';
 
 const AllTheProviders = ({ children }: { children: React.ReactNode }) => (
-  <ThemeProvider theme={theme}>{children}</ThemeProvider>
+  <SearchProvider>
+    <ThemeProvider theme={theme}>{children}</ThemeProvider>
+  </SearchProvider>
 );
 
 const customRender = (ui: ReactElement, options?: Omit<RenderOptions, 'wrapper'>) =>
