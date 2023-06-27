f2f343359f9372011aa29348c254618a8da67378
SEO <108770949+Seo0H@users.noreply.github.com>
2023-06-27 21:45:10 +0900

    test: /main 페이지 테스트 코드 추가
    잘못되어있던 waitFor 코드 삭제
    온보딩 말풍선 test 코드 추가

--- src/__test__/integration/main-localstorage-dependencies/index.test.tsx
+++ src/__test__/integration/main-localstorage-dependencies/index.test.tsx
@@ -1,50 +1,49 @@
 import mockRouter from 'next-router-mock';
 
 import { fireEvent, render, screen, waitFor } from '@/__test__/utils/customRender';
-import { ONBOARDING, VISITED } from '@/constant/localstorage';
+import { ONBOARDING, VISITED, LocalStorageKeys } from '@/constant/localstorage';
 import * as useLocalStorage from '@/hooks/useLocalStorage';
 import Home from '@/pages';
 
 jest.mock('@/hooks/useLocalStorage');
 const localStorageMock = jest.spyOn(useLocalStorage, 'default');
 
-describe('main 페이지와 로컬스토리지 의존성 테스트', () => {
-  it('로컬스토리지의 VISITED가 false일 경우, ABOUT 페이지로 이동해야 한다', async () => {
-    localStorageMock.mockImplementation(() => [VISITED.status.FALSE, () => '_']);
-
-    render(<Home />);
-    waitFor(() => expect(mockRouter.asPath).toBe('/about'));
-  });
-
-  it('로컬스토리지의 VISITED가 DATE 일 경우 MAIN PAGE에 머물러야 한다.', async () => {
-    localStorageMock.mockImplementation(() => [VISITED.status.DATE, () => '_']);
-
-    render(<Home />);
-    waitFor(() => expect(mockRouter.asPath).toBe('/'));
-  });
-
-  it('로컬스토리지의 ONBOARDING이 NOT_WATCHED일 경우 Onboarding이 보여져야 한다.', async () => {
-    localStorageMock
-      .mockImplementationOnce(() => [VISITED.status.DATE, () => '_'])
-      .mockImplementation(() => [ONBOARDING.status.NOT_WATCHED, () => '_']);
-
-    const { findByText, findAllByRole } = render(<Home />);
-    waitFor(() => {
-      expect(findByText).toBe(' 사고 싶은 물건을 입력하세요!');
-      expect(findByText).toBe('버튼을 눌러 검색하세요!');
-      expect(findAllByRole('button')).toBe('시작하기');
+describe('main 페이지 정상 작동 테스트', () => {
+  describe('로컬스토리지 VISITED 값 변화에 따른 테스트', () => {
+    it('로컬스토리지의 VISITED가 DATE 일 경우 MAIN PAGE에 머물러야 한다.', () => {
+      localStorageMock.mockReturnValue([VISITED.status.DATE, () => '_']);
+      render(<Home />);
+      expect(mockRouter.asPath).toBe('/');
+    });
+    it('로컬스토리지의 VISITED가 false일 경우, ABOUT 페이지로 이동해야 한다', () => {
+      localStorageMock.mockReturnValue([VISITED.status.FALSE, () => '_']);
+      render(<Home />);
+      expect(mockRouter.asPath).toBe('/about');
     });
   });
 
-  it('ONBOARDING의 "시작하기" 버튼을 누를 경우 Onboarding이 사라져야 한다.', () => {
-    localStorageMock
-      .mockImplementationOnce(() => [VISITED.status.DATE, () => '_'])
-      .mockImplementation(() => [ONBOARDING.status.NOT_WATCHED, () => '_']);
+  describe('로컬스토리지 ONBOARDING 값 변화에 따른 테스트', () => {
+    it('로컬스토리지의 ONBOARDING이 NOT_WATCHED일 경우 Onboarding이 보여져야 한다.', () => {
+      localStorageMock
+        .mockImplementationOnce(() => [VISITED.status.DATE, () => '_'])
+        .mockImplementation(() => [ONBOARDING.status.NOT_WATCHED, () => '_']);
 
-    render(<Home />);
-    waitFor(() => {
-      fireEvent.click(screen.getByRole('button', { name: '시작하기' }));
-      expect(screen.findByText).not.toBe(' 사고 싶은 물건을 입력하세요!');
+      const { findByText, findAllByRole } = render(<Home />);
+      expect(findByText(' 사고 싶은 물건을 입력하세요!')).toBeTruthy();
+      expect(findByText('버튼을 눌러 검색하세요!')).toBeTruthy();
+      expect(findAllByRole('button', { description: '시작하기' })).toBeTruthy();
+    });
+
+    it('ONBOARDING의 "시작하기" 버튼을 누를 경우 온보딩 말풍선이 떠야 한다.', () => {
+      localStorageMock.mockImplementation((key: LocalStorageKeys, value: string) => {
+        if (key === VISITED.key) return [VISITED.status.DATE, () => '_'];
+        return [value, (newState: string) => window.localStorage.setItem(ONBOARDING.key, newState)];
+      });
+
+      render(<Home />);
+      expect(screen.findByText(/사고 싶은 물건을 입력하세요!/)).toBeTruthy();
+      fireEvent.click(screen.getByLabelText('FadeInBackground'));
+      expect(screen.getByAltText(/onboarding/)).toBeTruthy();
     });
   });
 });
