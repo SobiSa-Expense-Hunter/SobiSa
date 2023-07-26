4416be4ca278a73543914537e82807b97776a2e1
seo <emma05127@gmail.com>
2023-07-26 16:55:59 +0900

    [#174] fix: 유저검색내역 하단 이미지 가시 오류 수정
    이미지가 보였다 보이지 않았다 하던 문제
    부모 컴포넌트의 absolute로 인해 발생
    해당 컴포넌트 삭제 후 image 자체에 고정 값 부여
    해결 관련 url : https://www.notion.so/seo0h/image-chrom-a8089dde7aa44692b558ff1d7402ff8b?pvs=4

--- src/components/common/Header/search-history/UserResearchFormCard.tsx
+++ src/components/common/Header/search-history/UserResearchFormCard.tsx
@@ -9,12 +9,7 @@ const UserResearchFormCard = () => {
   return (
     <Style.UserFormHref padding='20px 32px' width='100%' height='auto' gap='12px'>
       <Layout.HStack width='100%'>
-        <Layout.VStack
-          alignItems='flex-start'
-          justifyContent='center'
-          width='auto'
-          style={{ flex: 'none' }}
-        >
+        <Layout.VStack alignItems='flex-start' width='auto' style={{ flex: 'none' }}>
           <Style.TagRoundOrang padding='0px 8px' margin='0 0 8px'>
             <Font.SmallOrange>수정제의</Font.SmallOrange>
           </Style.TagRoundOrang>
@@ -22,23 +17,14 @@ const UserResearchFormCard = () => {
         </Layout.VStack>
 
         {/* MEMO: 디자인 상 이미지가 flex에 정렬되어 있지 않아,
-            마진을 주어 시각적 보정을 해주었음 */}
+            마진 + flex-end 을 주어 시각적 보정을 해주었음 */}
         <Layout.VStack alignItems='flex-end' width='100%' margin='-12px -12px 0 0'>
-          <Layout.Box
-            width='100%'
-            height='100%'
-            maxWidth='63px'
-            maxHeight='70px'
-            position='absolute'
-            style={{ zIndex: 1 }}
-          >
-            <Image
-              src='assets/image/search-input/sobisa-with-sunglass-and-hello.webp'
-              fill
-              alt='sobisa-with-sunglass-and-hello'
-              style={{ objectFit: 'contain' }}
-            />
-          </Layout.Box>
+          <Image
+            src='assets/image/search-input/sobisa-with-sunglass-and-hello.webp'
+            width={63}
+            height={70}
+            alt='sobisa-with-sunglass-and-hello'
+          />
         </Layout.VStack>
       </Layout.HStack>
 
