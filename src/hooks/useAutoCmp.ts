44b781b64e08f27007788403074deb791effb2d7
seo <emma05127@gmail.com>
2023-09-21 22:21:56 +0900

    feature: 자동완성 debounce 추가

--- src/hooks/useAutoCmp.ts
+++ src/hooks/useAutoCmp.ts
@@ -1,3 +1,4 @@
+/* eslint-disable consistent-return */
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useState, useCallback } from 'react';
 
@@ -21,19 +22,29 @@ function useAutoCmp(params: string) {
   useEffect(() => {
     if (!params) return;
 
-    // !DESCRIBE : 로딩스피너가 너무 빠르게 동작하면 깜빡거리듯 보여지기에 0.5초가 지나고 로딩 상태로 변경
-    const loadingTimeout = setTimeout(() => setState(STATE.LOADING), 500);
+    let debounceLoadingSpinner: ReturnType<typeof setTimeout>;
+    let debounceParams: ReturnType<typeof setTimeout>;
 
     try {
-      fetchData().then(res => {
-        setAutoCmpList(makeAutoCmpList(res).slice(0, 8));
-        clearTimeout(loadingTimeout);
-        setState(STATE.SUCCESS);
-      });
+      debounceLoadingSpinner = setTimeout(() => setState(STATE.LOADING), 500);
+      debounceParams = setTimeout(
+        () =>
+          fetchData().then(res => {
+            setAutoCmpList(makeAutoCmpList(res).slice(0, 8));
+            clearTimeout(debounceLoadingSpinner);
+            setState(STATE.SUCCESS);
+          }),
+        1000,
+      );
     } catch (error) {
-      clearTimeout(loadingTimeout);
       setState(STATE.ERROR);
     }
+
+    return () => {
+      setAutoCmpList(['']);
+      clearTimeout(debounceLoadingSpinner);
+      clearTimeout(debounceParams);
+    };
   }, [params, mode]);
 
   const fetchData = useCallback(async () => {
